1eef7b37d554121106318f0ef2124fcf38ace473
James Adams <33360278+nacklebert@users.noreply.github.com>
2017-11-20 11:24:29 -0800

    Update config.js

--- example/config.js
+++ example/config.js
@@ -1,8 +1,8 @@
 // defines all availa auth options, though all are not required...
 module.exports = {
-  siteID: process.env.SITE_ID || '1234', // optional
-  siteName: process.env.SITE_NAME || 'acme',
-  webExID: process.env.SECURE_ADMIN_ID || 'admin@acme.com',
+  siteID: process.env.SITE_ID || '1234', // either siteID or siteName required, if both WebEx uses siteName
+  siteName: process.env.SITE_NAME || 'acme', // either siteID or siteName required, if both WebEx uses siteName
+  webExID: process.env.SECURE_ADMIN_ID || 'admin@acme.com', //// If site is Common Identity (CI), webExID must be an email address
   email: process.env.SECURE_ADMIN_EMAIL || 'admin@acme.com', // optional
   password: process.env.SECURE_ADMIN_PASSWORD || 's3cret', // password or session ticket required
   partnerID: process.env.SECURE_PARTNER_ID || '5678', // optional
